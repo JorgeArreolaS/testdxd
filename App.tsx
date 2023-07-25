d55ccf1e199ba3bba800550537f1c532655daf59
Jorge Arreola <jorge@arreola.studio>
2023-07-25 12:04:10 -0600

    Tests

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Page from './Page';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Chi owo</Text>
       <StatusBar style="auto" />
+      <Page/>
     </View>
   );
 }
